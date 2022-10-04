ea3b1bb66138f8a7237d245b560ef0d371141dd6
Mark Palinkas <p.mark9807@gmail.com>
2022-10-04 14:24:46 +0200

    Initializing project

---
+++ src/main/java/com/codecool/app/ElProyecteGrandeApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.app;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElProyecteGrandeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElProyecteGrandeApplication.class, args);
+    }
+}
