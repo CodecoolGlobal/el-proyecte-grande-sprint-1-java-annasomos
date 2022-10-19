ab109c5c1d8e90bf79e929df323ca087aabecc46
HorvathAron <horvatharon9431@gmail.com>
2022-10-19 09:17:45 +0200

    Completed LogIn.js

--- src/main/java/com/codecool/app/frontend/mytreat/src/App.js
+++ src/main/java/com/codecool/app/frontend/mytreat/src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
+import LogIn from './LogIn';
 import {Routes, Route, useNavigate} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Layout/>}>
+          {/* <Route path='/' element={<Layout/>}>
           <Route index element={<Home/>}/>
           <Route path='/friends' element={<Users/>}/>
           <Route path='/user/:userId' element={<User/>}/>
           <Route path='/pubs' element={<Pubs/>}/>
-          <Route path='/pubs/:pubId' element={<Pub/>}/>
+          <Route path='/pubs/:pubId' element={<Pub/>}/> */}
           <Route path='/login' element={<LogIn/>}/>
-          <Route path='/registration' element={<Registration/>}/>
-          <Route path='/add-friend' element={<AddFriend/>}/>
-        </Route>
+          {/* <Route path='/registration' element={<Registration/>}/>
+          <Route path='/add-friend' element={<AddFriend/>}/> */}
       </Routes>
     </div>
   );
