b8eab43a3e9e00d69a3c45340152d8fccfd9e300
Mark Palinkas <p.mark9807@gmail.com>
2022-10-19 10:30:37 +0200

    create layout

--- src/main/java/com/codecool/app/frontend/mytreat/src/App.js
+++ src/main/java/com/codecool/app/frontend/mytreat/src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
-import {Routes, Route, useNavigate} from 'react-router-dom';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Header from "./Header";
+import Layout from "./Layout";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
+      <Routes path="/">
+        <Route index element={<Home />} />
+      </Routes>
+      {/* <Routes>
         <Route path='/' element={<Layout/>}>
           <Route index element={<Home/>}/>
           <Route path='/friends' element={<Users/>}/>
@@ -15,7 +21,7 @@ function App() {
           <Route path='/registration' element={<Registration/>}/>
           <Route path='/add-friend' element={<AddFriend/>}/>
         </Route>
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
