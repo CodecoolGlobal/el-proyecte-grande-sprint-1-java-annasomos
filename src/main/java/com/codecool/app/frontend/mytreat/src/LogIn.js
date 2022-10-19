17923f9bb5a04ce8f148aa59735bf123be327ed5
HorvathAron <horvatharon9431@gmail.com>
2022-10-19 10:00:03 +0200

    Create Registration page

--- src/main/java/com/codecool/app/frontend/mytreat/src/LogIn.js
+++ src/main/java/com/codecool/app/frontend/mytreat/src/LogIn.js
@@ -4,11 +4,11 @@ const LogIn = () => {
     return(
         <form>
           <label>
-            <p>Username</p>
+            <p>Username:</p>
             <input type="text" />
           </label>
           <label>
-            <p>Password</p>
+            <p>Password:</p>
             <input type="password" />
           </label>
           <div>
--- src/main/java/com/codecool/app/frontend/mytreat/src/Registration.js
+++ src/main/java/com/codecool/app/frontend/mytreat/src/Registration.js
@@ -1,21 +1,25 @@
 import React from 'react'
 
 const Registration = () => {
-    return(
-        <form>
-          <label>
-            <p>Friend code:</p>
-            <input type="text" />
-          </label>
-          <label>
-            <p>Your friend code:</p>
-            <p></p>
-          </label>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      )
+  return(
+    <form>
+      <label>
+        <p>Username:</p>
+        <input type="text" />
+      </label>
+      <label>
+        <p>E-mail:</p>
+        <input type="text" />
+      </label>
+      <label>
+        <p>Password:</p>
+        <input type="password" />
+      </label>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  )
 }
 
 export default Registration
